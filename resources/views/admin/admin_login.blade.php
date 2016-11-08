740154ae2bd6add21c26a703ef70489f9249b8fb
venkat24 <venkatramansrikanth@outlook.com>
2016-11-08 13:23:21 +0000

    Added admin panel login

---
+++ resources/views/admin/admin_login.blade.php
@@ -0,0 +1,102 @@
+<html>
+
+<head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+  <style>
+    body {
+      display: flex;
+      min-height: 100vh;
+      flex-direction: column;
+    }
+
+    main {
+      flex: 1 0 auto;
+    }
+
+    body {
+      background: #fff;
+    }
+
+    .input-field input[type=date]:focus + label,
+    .input-field input[type=text]:focus + label,
+    .input-field input[type=email]:focus + label,
+    .input-field input[type=password]:focus + label {
+      color: #e91e63;
+    }
+
+    .input-field input[type=date]:focus,
+    .input-field input[type=text]:focus,
+    .input-field input[type=email]:focus,
+    .input-field input[type=password]:focus {
+      border-bottom: 2px solid #e91e63;
+      box-shadow: none;
+    }
+  </style>
+</head>
+        <script type="text/javascript">
+           var SITE_BASE_URL = "{{ url(env('SITE_BASE_URL'))}}";
+        </script>
+<body>
+  <div class="section"></div>
+  <main>
+    <center>
+      <div class="section"></div>
+      <img src="{{ asset('images/aaveglogocoloured.png') }}" width="300px">
+      <h5 class="teal-text text-accent-4"><b>Admin Login</b></h5>
+      <div class="section"></div>
+
+      <div class="container">
+        <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
+
+          <form class="col s12" method="post" action="javascript: location.href = (SITE_BASE_URL + '/admin/login')">
+            <div class='row'>
+              <div class='col s12'>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+                <input type='text' name='email' id='username' />
+                <label>Username</label>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+                <input class='validate' type='password' name='password' id='password' />
+                <label for='password'>Password</label>
+              </div>
+              <label style='float: right;'>
+                                <a class='teal-text' href='#!'><b>Forgot Password?</b></a>
+                            </label>
+            </div>
+            <br />
+            <center>
+              <div class='row'>
+                <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect cyan darken-3' id="login_button">Login</button>
+              </div>
+                <div class="progress" id="loading" style="display:none">
+                    <div class="indeterminate"></div>
+                </div>
+            </center>
+          </form>
+        </div>
+      </div>
+    </center>
+
+    <div class="section"></div>
+    <div class="section"></div>
+  </main>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+</body>
+      <footer class="page-footer teal accent-4">
+          <div class="footer-copyright">
+            <div class="container center">
+            <a class="grey-text text-lighten-4 center" href="#!">Made with ❤ by Aaveg Web Team</a>
+            </div>
+          </div>
+        </footer>
+</html>
