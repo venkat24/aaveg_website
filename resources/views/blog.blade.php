0f9306d092eb98aa4afeb6cc7ccd656101678ac0
venkat24 <venkatramansrikanth@outlook.com>
2016-11-08 13:28:35 +0000

    Added basic blog layout, without JS

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		.single-logo {
+			padding-top: 40px;
+			padding-bottom: 40px;
+			padding-left: 32px;
+			padding-right: 32px;
+		}
+		body {
+			color: #5f5f5f;
+			background-color: #ececec;
+			word-wrap: break-word;
+		}
+		.sub-post {
+			/* width:  33%; */
+		}
+	</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<title>
+		AavegBlog
+	</title>
+</head>
+<body>
+	<header class="blue-grey lighten-1">
+		<div class="container position-relative">
+			<div class="single-logo center-align">
+				<a href="#">
+					<img src="aavegbloglogo.png" width="400">
+				</a>
+			</div>
+		</div>
+	</header>
+	<div class="main-content">
+	<div class="container">
+		<div class="row" style="padding-top:20px;">
+			<div class="col s12 l9">
+			<div class="row">
+				<div class="col s12">
+					<div class="card">
+						<div class="card-image waves-effect waves-block waves-light">
+							<img class="activator" src="{{asset('deadpool.jpg')}}">
+						</div>
+						<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">Read More!</a></p>
+						</div>
+						<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
+							<p>Deadpool is the greatest anti-hero of all time.</p>
+						</div>
+					  </div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 l6">
+					<div class="card">
+						<div class="card-image waves-effect waves-block waves-light">
+							<img class="activator" src="deadpool.jpg">
+						</div>
+						<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">Read More!</a></p>
+						</div>
+						<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
+							<p>Deadpool is the greatest anti-hero of all time.</p>
+						</div>
+					  </div>
+				</div>
+				<div class="col s12 l6">
+					<div class="card">
+						<div class="card-image waves-effect waves-block waves-light">
+							<img class="activator" src="deadpool.jpg">
+						</div>
+						<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">Read More!</a></p>
+						</div>
+						<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
+							<p>Deadpool is the greatest anti-hero of all time.</p>
+						</div>
+					  </div>
+				</div>
+				<div class="col s12 l6">
+					<div class="card">
+						<div class="card-image waves-effect waves-block waves-light">
+							<img class="activator" src="deadpool.jpg">
+						</div>
+						<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">Read More!</a></p>
+						</div>
+						<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
+							<p>Deadpool is the greatest anti-hero of all time.</p>
+						</div>
+					  </div>
+				</div>
+				<div class="col s12 l6">
+					<div class="card">
+						<div class="card-image waves-effect waves-block waves-light">
+							<img class="activator" src="deadpool.jpg">
+						</div>
+						<div class="card-content">
+							<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
+							<p><a href="#">Read More!</a></p>
+						</div>
+						<div class="card-reveal">
+							<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
+							<p>Deadpool is the greatest anti-hero of all time.</p>
+						</div>
+					  </div>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 l3">
+		  <div class="card blue-grey ligten-3">
+            <div class="card-content white-text">
+              <span class="card-title"><h4>The Blog</h4></span>
+              <p class="flow-text">The blog is a new initiative of Aaveg '17. The content team will be blogging about the happenings of Aaveg. <br /><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget lacus ut mi rhoncus auctor. Aliquam bibendum magna arcu. Nullam suscipit, ligula eget consectetur finibus, sem ipsum cursus nulla, ac rutrum felis purus a justo.<br /></p>
+            </div>
+          </div>
+          <div class="card brown ligten-2">
+            <div class="card-content white-text">
+              <span class="card-title">More Links</span>
+              <p>Here are some links to more of our content.
+              Nam volutpat gravida lorem nec laoreet. Maecenas elementum tempus consectetur. Integer magna odio, dignissim in iaculis vel, varius at velit. Integer eu enim ipsum. Maecenas sed turpis ut arcu cursus lobortis faucibus eu lectus. Morbi vel eleifend nisl, eget sodales magna. 
+              </p>
+            </div>
+            <div class="card-action">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+            </div>
+          </div>
+		</div>
+	</div>
+	</div>
+	</div>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+</body>
+</html>
