b9edc018e7164f46c18a5fa51d2ab9aca2090eef
venkat24 <venkatramansrikanth@outlook.com>
2016-11-09 01:38:54 +0000

    Changes made to new blog post JS

--- public/js/new_post.js
+++ public/js/new_post.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
   $('select').material_select();
-  $('#trumbowyg').trumbowyg();
+  // $('#trumbowyg').trumbowyg();
 });
 
-window.setInterval(function(){
-	// console.log($('#trumbowyg').html());
-	console.log($('#main-form').serialize());
-},1000);
+function uploadBlog() {
+	var route = SITE_BASE_URL + '/admin/blog/newpost';
+	var method = 'POST';
+	var active = 0;
+	if($("#active").is(":checked")){
+		active=1;
+	}
+	console.log($("#image")[0]);
+	var body = new FormData();
+	body.append("title", $("#title").val());
+	body.append("subtitle", $("#subtitle").val());
+	body.append("author_name", $("#author").val());
+	body.append("image", $('#image')[0]);
+	body.append("content", $("#content").val());
+	body.append("active", active);
+	console.log(body);
+	var request = $.ajax({
+		url: route,
+		method: method,
+		type: "POST",
+		processData: false,
+		contentType: false,
+		enctype: 'multipart/form-data',
+		data: body
+	});
+	request.done(function(data){
+		if(data.status_code == 200) {
+			conosole.log("Success!")
+			// location.href = SITE_BASE_URL + "/admin/home";
+		} else {
+			alert("Upload Failed");
+		}
+	});
+}
