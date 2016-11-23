ae83da7e01c163d72207938cb84ffdca32195159
venkat24 <venkatramansrikanth@outlook.com>
2016-11-23 16:09:33 +0000

    Modified base to add About Aaveg entry

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -30,7 +30,8 @@
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/scoreboard">Scoreboard<span class="sr-only">(current)</span></a></li>
+        <li><a href="/about">About Aaveg<span class="sr-only"></span></a></li>
+        <li><a href="/scoreboard">Scoreboard<span class="sr-only"></span></a></li>
         <li><a href="/blog">Blog</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
