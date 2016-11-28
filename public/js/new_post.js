7852979150034393ee7b359c96c6645d3dbad57a
venkat24 <venkatramansrikanth@outlook.com>
2016-11-28 20:04:51 +0000

    Added Author Dropdown in New Blog post

--- public/js/new_post.js
+++ public/js/new_post.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('select').material_select();
+  setAuthors();
 });
 
 function uploadBlog() {
@@ -21,6 +22,27 @@ function uploadBlog() {
 		if(data.status_code == 200) {
 			Materialize.toast('Post Added!', 4000, 'rounded');
 			//location.reload();
+		} else {
+			Materialize.toast('Upload Failed!', 4000, 'rounded');
+			console.log(data.status_code + " " + data.message);
+		}
+	});
+}
+
+function setAuthors () {
+	var route = SITE_BASE_URL + '/blog/getauthors';
+	var method = 'POST';
+	var request = $.ajax({
+		url: route,
+		method: method,
+	});
+	request.done(function(data){
+		if(data.status_code == 200) {
+		    var source = $('#author-name-dropdown').html();
+		    var template = Handlebars.compile(source);
+		    var html = template(data);
+		    $('#author-name').append(html);
+		    $('select').material_select();
 		} else {
 			Materialize.toast('Upload Failed!', 4000, 'rounded');
 			console.log(data.status_code + " " + data.message);
