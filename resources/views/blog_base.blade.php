cdd8446e24c0ab9bcc0f9bf4e24f206a35005b76
venkat24 <venkatramansrikanth@outlook.com>
2016-11-29 23:29:54 +0000

    Home button and modified footer bar in blog base

--- resources/views/blog_base.blade.php
+++ resources/views/blog_base.blade.php
@@ -3,8 +3,8 @@
 <head>
 	<style type="text/css">
 		.single-logo {
-			padding-top: 40px;
-			padding-bottom: 40px;
+			padding-top: 10px;
+			padding-bottom: 10px;
 			padding-left: 32px;
 			padding-right: 32px;
 		}
@@ -29,10 +29,24 @@
 		.main-image {
 			position: relative; 
 		}
+		.wrap {
+			  text-overflow: ellipsis;
+			  white-space: nowrap;
+			  overflow: hidden;
+		}
+		.go-back {
+			position: absolute;
+			top: 45px;
+			left: 3%;
+		}
 	</style>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	@yield('links')
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
+	<script type="text/javascript" src="{{asset('js/blog_base.js')}}"></script>
 	@yield('scripts')
 	<title>
 		AavegBlog
@@ -46,17 +60,20 @@
 	<header class="indigo lighten-1">
 		<div class="container position-relative">
 			<div class="single-logo center-align">
-				<a href="/">
+				<a href="/blog">
 					<img src="{{asset('aavegbloglogo.png')}}" width="400">
 				</a>
 			</div>
 		</div>
+		<a href="/" class="white-text go-back">
+			<i class="medium material-icons">home</i>
+		</a>
 	</header>
 
 	@yield('main')
 	
 	<div id="main-container"></div>
-<!--  	<footer class="page-footer">
+ 	<footer class="page-footer">
       <div class="container">
         <div class="row">
           <div class="col l6 s12">
@@ -65,21 +82,23 @@
           </div>
           <div class="col l4 offset-l2 s12">
             <h5 class="white-text">Popular Content</h5>
-            <ul>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
+            <div id="links-list-container"></div>
+            <script type="text/template" id="links-layout">
+	            <ul>
+		            @{{#each message}}
+		              <li><a class="grey-text text-lighten-3" href="/blog/single/@{{blog_id}}">@{{title}}</a></li>
+		            @{{/each}}
+	            </ul>
+            </script>
           </div>
         </div>
       </div>
       <div class="footer-copyright">
         <div class="container">
         ©2016<a class="grey-text text-lighten-4 center-align" href="/admin/login"> </a>Aaveg
-        <a class="grey-text text-lighten-4 right" href="#!">Archives</a>
+        <a class="grey-text text-lighten-4 right" href="/blog/archives">Archives</a>
         </div>
       </div>
-    </footer> -->
+    </footer>
 </body>
 </html>
