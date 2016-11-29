b4dee61aa2a7d7e3a6318c0e80335149f78006ac
venkat24 <venkatramansrikanth@outlook.com>
2016-11-29 23:28:52 +0000

    Main blog page with AJAX calls and Side Panes

---
+++ public/js/blog.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  getBlogPosts();
+});
+
+function getBlogPosts() {
+	var route = SITE_BASE_URL + '/blog/getLatestPosts';
+	var method = 'POST';
+	var body = {
+		'post_count': 5,
+	}
+
+	var request = $.ajax({
+		url: route,
+		method: method,
+		data: body
+	});
+
+	request.done(function(data){
+		if(data.status_code == 200) {
+			var main_pane = data.message.shift();
+
+			var source = $('#main-pane').html();
+			var template = Handlebars.compile(source);
+		    var html = template(main_pane);
+		    $('#main-pane-container').append(html);
+
+		    source = $('#blog-panes').html();
+			template = Handlebars.compile(source);
+		    html = template(data);
+		    $('#blog-panes-container').append(html);
+		} else {
+			console.log(data.status_code + " " + data.message);
+		}
+	});
+}
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,118 +1,87 @@
 @extends('blog_base')
+
+@section('links')
+@endsection
+
+@section('scripts')
+	<script type="text/javascript" src="{{asset('js/blog.js')}}"></script>
+@endsection
+
 @section('main')
-<!-- This is all dummy content -->
-<!-- <div class="main-content">
 <div class="container">
 	<div class="row" style="padding-top:20px;">
 		<div class="col s12 l9">
-		<div class="row">
+			<div class="row" id="main-pane-container">
+				<!-- Main post goes here -->
+			</div>
+			<div class="row" id="blog-panes-container">
+				<!-- Other panes go here -->
+			</div>
+		</div>
+
+		<div class="col s12 l3">
+		  <div class="card indigo lighten-2">
+	        <div class="card-content white-text">
+	          <p class="flow-text">Welcome to the Blog! <br> The latest addition to Aaveg, this blog gives you enthralling content relating to the Fresher life. Consider it your sneak peek into this spellbinding fest and more!<br /></p>
+	        </div>
+	      </div>
+	      <div class="card teal lighten-2">
+	        <div class="card-content white-text">
+	          <span class="card-title">Links</span>
+	          <p class="flow-text">Done reading? Here are some links to other parts of the website.
+	          </p>
+	        </div>
+	        <div class="card-action">
+	          <a href="/">Home</a>
+	          <a href="/scoreboard">Scores</a>
+	          <a href="/team">Team</a>
+	        </div>
+	      </div>
+		</div>
+	</div>
+</div>
+<script type="text/template" id="main-pane">
 			<div class="col s12">
 				<div class="card">
 					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="{{asset('deadpool.jpg')}}">
+						<img class="activator" src="@{{image_path}}">
 					</div>
 					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
-						<p><a href="#">Read More!</a></p>
+						<span class="card-title activator grey-text text-darken-4">@{{title}}<i class="material-icons right">more_vert</i></span>
+						<p><a href="/blog/single/@{{blog_id}}">Read More!</a></p>
 					</div>
 					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
-						<p>Deadpool is the greatest anti-hero of all time.</p>
+						<span class="card-title grey-text text-darken-4"><div class="indigo-text text-lighten-1">@{{title}}</div><i class="material-icons right">close</i></span>
+						<span><i>@{{author_name}}</i></span>
+						<p class="teal-text text-lighten-2">@{{subtitle}}</p>
+						<p class="wrap">@{{content}}</p>
+						<p><a href="/blog/single/@{{blog_id}}">Read More!</a></p>
 					</div>
 				  </div>
 			</div>
-		</div>
-		<div class="row">
+</script>
+<script type="text/template" id="blog-panes">
+			@{{#each message}}
 			<div class="col s12 l6">
 				<div class="card">
 					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="deadpool.jpg">
+						<img class="activator" src="@{{image_path}}">
 					</div>
 					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
-						<p><a href="#">Read More!</a></p>
+						<span class="card-title activator grey-text text-darken-4">@{{title}}<i class="material-icons right">more_vert</i></span>
+						<p><a href="/blog/single/@{{blog_id}}">Read More!</a></p>
 					</div>
 					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
-						<p>Deadpool is the greatest anti-hero of all time.</p>
+						<span class="card-title grey-text text-darken-4"><div class="indigo-text text-lighten-1">@{{title}}</div><i class="material-icons right">close</i></span>
+						<span><i>@{{author_name}}</i></span>
+						<p class="teal-text text-lighten-2">@{{subtitle}}</p>
+						<p class="wrap">@{{content}}</p>
+						<p><a href="/blog/single/@{{blog_id}}">Read More!</a></p>
 					</div>
 				  </div>
 			</div>
-			<div class="col s12 l6">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="deadpool.jpg">
-					</div>
-					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
-						<p><a href="#">Read More!</a></p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
-						<p>Deadpool is the greatest anti-hero of all time.</p>
-					</div>
-				  </div>
-			</div>
-			<div class="col s12 l6">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="deadpool.jpg">
-					</div>
-					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
-						<p><a href="#">Read More!</a></p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
-						<p>Deadpool is the greatest anti-hero of all time.</p>
-					</div>
-				  </div>
-			</div>
-			<div class="col s12 l6">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="deadpool.jpg">
-					</div>
-					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4">Deadpool Rocks!<i class="material-icons right">more_vert</i></span>
-						<p><a href="#">Read More!</a></p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Deadpool is Awesome<i class="material-icons right">close</i></span>
-						<p>Deadpool is the greatest anti-hero of all time.</p>
-					</div>
-				  </div>
-			</div>
-		</div>
-	</div>
-	<div class="col s12 l3">
-	  <div class="card indigo lighten-2">
-        <div class="card-content white-text">
-          <span class="card-title"><h4>The Blog</h4></span>
-          <p class="flow-text">The blog is a new initiative of Aaveg '17. The content team will be blogging about the happenings of Aaveg. <br /><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget lacus ut mi rhoncus auctor. Aliquam bibendum magna arcu. Nullam suscipit, ligula eget consectetur finibus, sem ipsum cursus nulla, ac rutrum felis purus a justo.<br /></p>
-        </div>
-      </div>
-      <div class="card teal lighten-2">
-        <div class="card-content white-text">
-          <span class="card-title">More Links</span>
-          <p>Here are some links to more of our content.
-          Nam volutpat gravida lorem nec laoreet. Maecenas elementum tempus consectetur. Integer magna odio, dignissim in iaculis vel, varius at velit. Integer eu enim ipsum. Maecenas sed turpis ut arcu cursus lobortis faucibus eu lectus. Morbi vel eleifend nisl, eget sodales magna. 
-          </p>
-        </div>
-        <div class="card-action">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-        </div>
-      </div>
-	</div>
-</div>
-</div>
-</div> -->
-<div class="container">
-	<div class="valign-wrapper">
-		<h1 class="coming-soon center-align">Coming Soon!</h1>
-	</div>
-</div>
+			@{{/each}}
+</script>
 @endsection
 @section('scripts')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
