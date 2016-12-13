3fa4ccefd468c9a0a32f7452bb97014a6c5704d4
venkat24 <venkatramansrikanth@outlook.com>
2016-12-13 22:38:39 +0530

    SEO

--- resources/views/blog_base.blade.php
+++ resources/views/blog_base.blade.php
@@ -58,6 +58,16 @@
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
 	<script type="text/javascript" src="{{asset('js/blog_base.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/jquery.dotdotdot.min.js')}}"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-88938333-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 	@yield('scripts')
 	<title>
 		Aaveg Blog
