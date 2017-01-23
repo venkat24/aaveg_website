6dee53e5ba98afed1d54c4136488dd623922dc9d
Anirudh Sundar <anirudhsundar98@github.com>
2017-01-23 11:23:30 +0530

    Fixed splash page media query

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -240,19 +240,4 @@
 
   </script>
   <script src="{{asset('js/events.js')}}"></script>
-
-  <script type="text/javascript">
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        canvas = document.getElementsByTagName('canvas')[0];
-        
-        window.addEventListener('resize', function() {
-            if (width != window.innerWidth || height != window.innerHeight) {
-                var width = window.innerWidth;
-                var height = window.innerHeight;
-                canvas.style.width = width + 'px';
-                canvas.style.height = height + 'px';
-            }
-        });
-  </script>
 @endsection
